1841ce89788a78f4ae849bb2f3c49c9ed13bf3d9
Suryakant <sk.zone900@gmaial.com>
2023-10-16 12:03:42 +0530

    New product feature has been added

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main()
     int b=2;
 int sum=a+b;
 // sum is added as new feture
+int product = (a*b);
+printf("sum is : %d", product);
 printf("sum is : %d", sum);
 printf("Hello gitt \n");
 printf("Check code in runway \n");
